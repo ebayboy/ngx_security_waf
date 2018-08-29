7bd7af3ec0a9774dbdbd944e22da723d8c3c3bbc
fanpengfei <fanpengfei@jd.com>
2018-08-29 17:04:39 +0800

    update for ipip test.c

---
+++ vendor/ipip/src/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+gcc -g test.c ipip.c  -o test
--- vendor/ipip/src/test.c
+++ vendor/ipip/src/test.c
@@ -23,25 +23,80 @@ char *strtok_r_2(char *str, char const *delims, char **context) {
     return ret;
 }
 
-int main() {
-    init("ipip.datx");
-    char *ip = "223.255.152.0";
-    char result[128];
+/* db format :
+ * [
+ * "GOOGLE.COM", // country_name
+ * "GOOGLE.COM", // region_name
+ * "",             // city_name
+ * "google.com", // owner_domain
+ * "level3.com", // isp_domain
+ * "", // latitude
+ * "", // longitude
+ * "", // timezone
+ * "", // utc_offset
+ * "", // china_admin_code
+ * "", // idd_code
+ * "", // country_code
+ * "", // continent_code
+ * "IDC", // idc
+ * "", // base_station
+ * "", // country_code3
+ * "", // european_union
+ * "", // currency_code
+ * "", // currency_name
+ * "ANYCAST" // anycast
+ * ]
+ **/
+
+int main(int argc, char **argv) {
+
+    char *path = "../../../conf/ipip_station.datx";
+    char *ip;
+    char result[128] = {0};
+    int i = 0;
+    char *title[] = {
+        "country_name",
+        "region_name",
+        "city_name",
+        "owner_domain",
+        "isp_domain",
+        "latitude",
+        "longitude",
+        "timezone",
+        "utc_offset",
+        "china_admin_code",
+        "idd_code",
+        "country_code",
+        "continent_code",
+        "idc",
+        "base_station",
+        "country_code3",
+        "european_union",
+        "currency_code",
+        "currency_name",
+        "anycast",
+    };
+
+    init(path);
+    if (argc > 1) {
+        ip = argv[1];     
+    } else {
+        ip = "123.121.117.72";
+    }
+
     find(ip, result);
-        printf("%s -> %s\n", ip, result);
-        char *rst = NULL;
-        char *lasts;
-        rst = strtok_r_2(result, "\t", &lasts);
-        while (rst) {
-            printf("%s\n", rst);
-            rst = strtok_r_2(NULL, "\t", &lasts);
-        }
+
+    printf("%s -> %s\n", ip, result);
+    char *rst = NULL;
+    char *lasts;
+    rst = strtok_r_2(result, "\t", &lasts);
+
+    for (i = 0;rst && i < 20;i++) {
+        printf("[%d][%s]:[%s]\n", i, title[i], rst);
+        rst = strtok_r_2(NULL, "\t", &lasts);
+    }
+
     destroy();
 
-    init("ipip.datx");
-    if (find("127.0.0.1", result)) {
-        printf("%s -> %s", "127.0.0.1", result);
-    }
-    destroy();
     return 0;
 }
